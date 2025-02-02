aa910852d9a22ff6b2dad35af0a576ef8b18785d
ddangmul <j.ddangmul@gmail.com>
2025-02-02 22:23:04 +0900

    스크롤 기반 애니메이션

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,24 +1,54 @@
 import { Link } from "react-router-dom";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 import cityImg from "../assets/city.jpg";
 import heroImg from "../assets/hero.png";
 
 export default function WelcomePage() {
+  const { scrollY } = useScroll(); // scrollY : 사용자가 스크롤을 내린 픽셀 수
+
+  // 변환할 값, [애니메이션을 먹일 값 중단점], [중단점에 먹일 애니메이션 값]
+  const yCity = useTransform(scrollY, [0, 200], [0, -100]);
+  const opacityCity = useTransform(
+    scrollY,
+    [0, 200, 300, 500],
+    [1, 0.7, 0.4, 0]
+  );
+  const yHero = useTransform(scrollY, [0, 200], [0, -150]);
+  const opacityHero = useTransform(scrollY, [0, 300, 500], [1, 1, 0]);
+  const yText = useTransform(scrollY, [0, 200, 300, 500], [0, 50, 50, 300]);
+  const scaleText = useTransform(scrollY, [0, 300], [1, 1.5]);
+
   return (
     <>
       <header id="welcome-header">
-        <div id="welcome-header-content">
+        <motion.div
+          id="welcome-header-content"
+          style={{ scale: scaleText, y: yText }}
+        >
           <h1>Ready for a challenge?</h1>
           <Link id="cta-link" to="/challenges">
             Get Started
           </Link>
-        </div>
-        <img
+        </motion.div>
+        <motion.img
+          style={{
+            // 향상된 style 속성 : 값이 변화할 때 프레이머 모션이 자동 플레이해 새 값을 반영
+            // useTransform에서 얻은 값과 함께 사용해야 함
+            // 값이 변할 때 컴포넌트 함수는 다시 실행되지 않음
+            opacity: opacityCity,
+            y: yCity,
+          }}
           src={cityImg}
           alt="A city skyline touched by sunlight"
           id="city-image"
         />
-        <img src={heroImg} alt="A superhero wearing a cape" id="hero-image" />
+        <motion.img
+          style={{ y: yHero, opacity: opacityHero }}
+          src={heroImg}
+          alt="A superhero wearing a cape"
+          id="hero-image"
+        />
       </header>
       <main id="welcome-content">
         <section>
